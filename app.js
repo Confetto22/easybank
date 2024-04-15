facfbaa8780f335afaa5b19af5466ebcf756cd36
Confetto <enockagyei1999@gmail.com>
2024-04-15 14:11:35 +0000

    first commit

---
+++ app.js
@@ -0,0 +1,20 @@
+const menuBt = document.querySelector('.hamburger')
+const topBar = document.querySelector('.bar.top')
+const middleBar = document.querySelector('.bar.middle')
+const bottomBar = document.querySelector('.bar.bottom')
+const menuWindow = document.querySelector('.menu_panel')
+const slidePart = document.querySelector('.intro_img')
+
+
+function animateMenu() {
+    menuWindow.classList.toggle('menu_animate')
+    slidePart.classList.toggle('intro_animate')
+}
+
+menuBt.onclick = () => {
+    topBar.classList.toggle('top_animate')
+    middleBar.classList.toggle('middle_animate')
+    bottomBar.classList.toggle('bottom_animate')
+    animateMenu();
+
+}
